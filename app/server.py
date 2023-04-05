8ee5078229c44e6ad4536cf217654ff569335446
miraedbswo <miraedbswo@gmail.com>
2023-04-05 23:55:03 +0900

    feature: ADD FastAPI app initalizer

---
+++ app/server.py
@@ -0,0 +1,88 @@
+from typing import List
+
+from fastapi import FastAPI, Request, Depends
+from fastapi.middleware import Middleware
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+from api import router
+from core.exceptions import CustomException
+from core.fastapi.dependencies import Logging
+from core.fastapi.middlewares import (
+    SQLAlchemyMiddleware,
+    ResponseLogMiddleware,
+)
+from core.fastapi.middlewares.authentication import AuthenticationMiddleware, AuthBackend
+
+
+def init_routers(app_: FastAPI) -> None:
+    app_.include_router(router, prefix="/api")
+
+
+def init_listeners(app_: FastAPI) -> None:
+    # Exception handler
+    @app_.exception_handler(CustomException)
+    async def custom_exception_handler(request: Request, exc: CustomException):
+        return JSONResponse(
+            status_code=exc.code,
+            content={"error_code": exc.error_code, "message": exc.message},
+        )
+
+
+def on_auth_error(request: Request, exc: Exception):
+    status_code, error_code, message = 401, None, str(exc)
+    if isinstance(exc, CustomException):
+        status_code = int(exc.code)
+        error_code = exc.error_code
+        message = exc.message
+
+    return JSONResponse(
+        status_code=status_code,
+        content={"error_code": error_code, "message": message},
+    )
+
+
+def make_middleware() -> List[Middleware]:
+    middleware = [
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        ),
+        Middleware(
+            AuthenticationMiddleware,
+            backend=AuthBackend(),
+            on_error=on_auth_error,
+        ),
+        Middleware(SQLAlchemyMiddleware),
+        Middleware(ResponseLogMiddleware),
+    ]
+    return middleware
+
+
+def create_app() -> FastAPI:
+    app_ = FastAPI(
+        title="Decorate TreeSchema",
+        description="API",
+        version="1.0.0",
+        docs_url="/docs",
+        redoc_url="/redoc",
+        dependencies=[Depends(Logging)],
+        middleware=make_middleware(),
+    )
+    init_routers(app_=app_)
+    init_listeners(app_=app_)
+    return app_
+
+
+app = create_app()
+
+
+# @app.on_event("startup")
+# async def init_tables():
+#     engine = engines.get("writer")
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
+#         await conn.run_sync(Base.metadata.create_all)
