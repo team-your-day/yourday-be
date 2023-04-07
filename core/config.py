74883c401845650d879f6e91ae011e7fc4601160
miraedbswo <miraedbswo@gmail.com>
2023-04-07 13:33:51 +0900

    feature: add secret key default for development

--- core/config.py
+++ core/config.py
@@ -10,14 +10,14 @@ class Config(BaseSettings):
     APP_PORT: int = 8000
     WRITER_DB_URL: str = f"mysql+aiomysql://user:pw@db_url:3306/yourday"
     READER_DB_URL: str = f"mysql+aiomysql://user:pw@db_url:3306/yourday"
-    JWT_SECRET_KEY: str = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY: str = os.getenv("SECRET_KEY", "5dmhxb*_x9^acw+6zoi0e3!g_vlmkbc(^+at%fnp$44+7&^c")
     JWT_ALGORITHM: str = "HS256"
     SENTRY_SDN: str = None
 
 
 class DevelopmentConfig(Config):
-    WRITER_DB_URL: str = f"mysql+aiomysql://admin:pw@database-1.cztglubqcnqf.ap-northeast-2.rds.amazonaws.com:3306/yourday"
-    READER_DB_URL: str = f"mysql+aiomysql://admin:pw@database-1.cztglubqcnqf.ap-northeast-2.rds.amazonaws.com:3306/yourday"
+    WRITER_DB_URL: str = f"mysql+aiomysql://admin:masterpassword@database-2.cztglubqcnqf.ap-northeast-2.rds.amazonaws.com:3306/yourday"
+    READER_DB_URL: str = f"mysql+aiomysql://admin:masteroassword@database-2.cztglubqcnqf.ap-northeast-2.rds.amazonaws.com:3306/yourday"
     REDIS_HOST: str = "redis"
     REDIS_PORT: int = 6379
 
